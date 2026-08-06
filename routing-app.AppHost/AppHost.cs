bf67edb7b2513b53a015e8bae2e528cded1b788f
james-e-crandall <james.e.crandall@gmail.com>
2026-08-06 13:43:27 -0500

    add angular

--- routing-app.AppHost/AppHost.cs
+++ routing-app.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var angularApp = builder.AddJavaScriptApp("angularApp", "../angular-website", runScriptName: "start")
+    .WithNpm(installCommand: "ci")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 builder.Build().Run();
